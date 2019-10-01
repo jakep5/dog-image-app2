dc38aeea19eff50aace4ff456603a33e22caa099
jakep5 <jakepagel1@yahoo.com>
2019-10-01 16:58:08 -0500

    finalized app

--- index.js
+++ index.js
@@ -9,9 +9,22 @@ function watchForm() {
 function getImages(images) {
     fetch(`https://dog.ceo/api/breeds/image/random/${images}`)
         .then (response => response.json())
-        .then (responseJson => console.log(responseJson))
-        .catch(error => alert ('There was an error, please try again.'));
+        .then (responseJson =>
+            displayResults(responseJson))
+        /* .catch(error => alert ('There was an error, please try again.')); */
 }
+
+function displayResults(responseJson) {
+    console.log(responseJson.message);
+    let arrayUrl = responseJson.message;
+    for (let i=0; i<arrayUrl.length;i++) {
+        $("div.images").append (
+            `<img src="${arrayUrl[i]}" class="dogImages">`
+        )
+    }
+};
+
+
 
 $(function() {
     console.log('Form is ready! Waiting for submit');
